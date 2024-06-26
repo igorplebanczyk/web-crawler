0b591cbbb2decd994a4578db01b0633c1cac019a
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-06-26 14:15:31 +0200

    Add main function and basic CLI argument processing

---
+++ main.js
@@ -0,0 +1,16 @@
+function main() {
+    if (process.argv.length < 3) {
+        console.log('Provide a URL to start crawling from.');
+        return;
+    } 
+    else if (process.argv.length > 3) {
+        console.log('Too many arguments');
+        return;
+    }
+    else {
+        const baseURL = process.argv[2];
+        console.log(`Starting crawl at ${baseURL}...`);
+    }
+}
+
+main();
