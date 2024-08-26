7fd54778fce3b48357f443c44b52ca73d435b02d
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-08-27 00:25:50 +0200

    Add cli input processing, update .gitignore

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	rawBaseURL := os.Args[1]
+
+	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
 }
