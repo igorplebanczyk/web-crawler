98ba0ff9ff73d1031499403aa54c9dc40902e804
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-10-12 11:25:08 +0200

    fix: fix broken usage

--- main.go
+++ main.go
@@ -8,23 +8,21 @@ import (
 func main() {
 	args := os.Args
 
+	// Check if at least 4 arguments are provided (os.Args includes the program name as the first argument)
 	if len(args) < 4 {
-		fmt.Println("no website provided")
+		fmt.Println("Usage: <program> <website> <max_concurrency> <max_pages>")
 		os.Exit(1)
 	}
 
-	if len(args) > 4 {
-		fmt.Println("too many arguments provided")
-		os.Exit(1)
-	}
+	rawBaseURL := args[1]
+	maxConcurrency := args[2]
+	maxPages := args[3]
 
-	rawBaseURL := os.Args[1]
-	maxConcurrency := os.Args[2]
-	maxPages := os.Args[3]
-
+	// Assuming getConfig is defined elsewhere and handles your settings
 	cfg, err := getConfig(rawBaseURL, maxConcurrency, maxPages)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	fmt.Printf("Starting crawl of: %s...\n", rawBaseURL)
